445208142b7b06d21e567354796bf70fe31dbf82
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2020-04-18 18:54:55 +0530

    'tutorial' added

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
 	.set('view engine', 'ejs')
 	.get('/', (req, res) => res.render('pages/index'))
+	.get('/index', (req, res) => res.redirect('/'))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 
